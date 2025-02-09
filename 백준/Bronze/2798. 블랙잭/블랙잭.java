7456918712bb023ddc7e397806576443b6377a74
prodksdb <150729023+prodksdb@users.noreply.github.com>
2025-02-09 10:34:36 +0900

    [Bronze II] Title: 블랙잭, Time: 68 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -3,40 +3,48 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String [] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String [] Input = br.readLine().trim().split(" ");
-		int N = Integer.parseInt(Input[0]);
-		int M = Integer.parseInt(Input[1]);
-		
-		String [] Input2 = br.readLine().trim().split(" ");
-		int [] Card = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			Card[i] = Integer.parseInt(Input2[i]);
+
+		String[] input = br.readLine().trim().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int M = Integer.parseInt(input[1]);
+		int[] arr = new int[N];
+		String[] input2 = br.readLine().trim().split(" ");
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(input2[i]);
 		}
-		
-		int result = 0;
-		
-		for(int i = 0; i < N; i++) {
-			for(int j = i+1; j< N; j++) {
-				for(int k = j+1; k < N; k++) {
-					int sum = Card[i]+ Card[j]+Card[k];
-					if(sum <= M) {
-						result = Math.max(sum, result);
+
+		Arrays.sort(arr);
+
+		int closetSum = 0;
+
+		for (int i = 0; i < N - 2; i++) {
+			int left = i + 1;
+			int right = N - 1;
+			while (left < right) {
+				int currentSum = arr[i] + arr[left] + arr[right];
+
+				if (currentSum <= M) {
+					closetSum = Math.max(closetSum, currentSum);
+					left++;
+					;
+				} else {
+					right--;
 				}
+
 			}
 		}
-		}
-		
-		bw.write(String.valueOf(result));
-		bw.newLine();
+
+		bw.write(String.valueOf(closetSum));
 		bw.flush();
 		br.close();
 		bw.close();
-		
 	}
+
 }
