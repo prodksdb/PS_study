126afed6d8b78c6ecd860b66e77b709dfc4e901f
prodksdb <150729023+prodksdb@users.noreply.github.com>
2025-03-18 16:10:40 +0900

    [D3] Title: 햄버거 다이어트, Time: 130 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,49 +1,52 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Solution {
 
-    public static int highScore;
-    public static int[] scores;
-    public static int[] calories;
-    public static int N, L;
+	static int[] scores;
+	static int[] calories;
+	static int N, L;
+	static int highScore;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for (int test_case = 1; test_case <= T; test_case++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            N = Integer.parseInt(st.nextToken());
-            L = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			N = Integer.parseInt(st.nextToken()); // 재료의 수
+			L = Integer.parseInt(st.nextToken()); // 제한 칼로리
 
-            scores = new int[N];
-            calories = new int[N];
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                scores[i] = Integer.parseInt(st.nextToken());
-                calories[i] = Integer.parseInt(st.nextToken());
-            }
+			scores = new int[N];
+			calories = new int[N];
 
-            highScore = 0;
-            select(0, 0, 0);
-            sb.append("#" + test_case + " " + highScore + "\n");
-        }
-        System.out.println(sb);
-    }
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				scores[i] = Integer.parseInt(st.nextToken());
+				calories[i] = Integer.parseInt(st.nextToken());
+			}
 
-    public static void select(int start, int score, int calorie) {
+			highScore = 0;
+			high(0, 0, 0);
+			sb.append("#" + test_case + " " + highScore + "\n");
+		}
+		System.out.println(sb);
+	}
 
-        if (calorie > L) return;
-        highScore = Math.max(highScore, score);
-        if (start == N) {
-            return;
-        }
+	// select : 재료 번호, calorie : 총 칼로리 수
+	public static void high(int number, int calorie, int score) {
 
-        select(start + 1, score + scores[start], calorie + calories[start]);
+		if (calorie > L)
+			return;
+		highScore = Math.max(score, highScore);
+		if (number == N)
+			return;
 
-        select(start + 1, score, calorie);
+		// 지금 재료 안 고를거야
+		high(number + 1, calorie, score);
 
-    }
+		// 지금 재료 고를거야
+		high(number + 1, calorie + calories[number], score + scores[number]);
+
+	}
 }
