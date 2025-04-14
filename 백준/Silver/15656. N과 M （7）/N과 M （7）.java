da3e17b0ffaca2190acff425b1ef12f0ca434cd5
prodksdb <150729023+prodksdb@users.noreply.github.com>
2025-04-14 17:45:51 +0900

    [Silver III] Title: N과 M (7), Time: 840 ms, Memory: 407848 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N, M;
+	static int[] numbers;
+	static int[] results;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		results = new int[M];
+		numbers = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(numbers);
+		Permutation(0);
+		System.out.println(sb);
+	}
+
+	public static void Permutation(int count) {
+		if (count == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(results[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			results[count] = numbers[i];
+			Permutation(count + 1);
+		}
+	}
+}
