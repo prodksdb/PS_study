287228bb0f596cf9ce2231a589781258b0da77eb
prodksdb <150729023+prodksdb@users.noreply.github.com>
2025-05-21 11:27:17 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,505 ms, Memory: 104,856 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/README.md
@@ -0,0 +1,15 @@
+# [D4] [Professional] 키 순서 - 5643 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXQsLWKd5cDFAUo) 
+
+### 성능 요약
+
+메모리: 104,856 KB, 시간: 1,505 ms, 코드길이: 1,351 Bytes
+
+### 제출 일자
+
+2025-05-21 11:27
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Solution {
+	static int N, M;
+	static int[][] students;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			N = Integer.parseInt(br.readLine());
+			M = Integer.parseInt(br.readLine());
+			students = new int[N + 1][N + 1];
+
+			for (int i = 0; i < M; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				students[a][b] = 1;
+			}
+
+			for (int k = 1; k <= N; k++) {
+				for (int i = 1; i <= N; i++) {
+					for (int j = 1; j <= N; j++) {
+						if (students[i][k] == 1 && students[k][j] == 1) {
+							students[i][j] = 1;
+						}
+					}
+				}
+			}
+
+			int result = 0;
+			for (int i = 1; i <= N; i++) {
+				int count = 0;
+				for (int j = 1; j <= N; j++) {
+					if (i == j)
+						continue;
+					if (students[i][j] == 1 || students[j][i] == 1) {
+						count++;
+					}
+
+				}
+				if (count == N - 1) {
+					result++;
+				}
+			}
+			sb.append("#" + test_case + " " + result + "\n");
+		}
+		System.out.println(sb);
+	}
+}
