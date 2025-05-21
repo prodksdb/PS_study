e898727c4c16b6c5f0671e60341b7f556aaae383
prodksdb <150729023+prodksdb@users.noreply.github.com>
2025-05-21 17:26:02 +0900

    [D2] Title: 나무 높이, Time: 85 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,54 +1,48 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
-	static int[] tree;
-
+class Solution{
+	
+	static int N;
+	static int [] tree;
+	static int highTree;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
-		int TC = Integer.parseInt(br.readLine());
-
-		for (int test_case = 1; test_case <= TC; test_case++) {
-			sb.append("#" + test_case + " ");
-			int N = Integer.parseInt(br.readLine());
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-			tree = new int[N];
-			int highTree = 0;
-			for (int i = 0; i < N; i++) {
+		int T= Integer.parseInt(br.readLine());
+		for(int test_case = 1; test_case <= T;test_case++) {
+			N = Integer.parseInt(br.readLine());
+            tree = new int[N];
+            highTree = Integer.MIN_VALUE;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < N; i++) {
 				tree[i] = Integer.parseInt(st.nextToken());
-				highTree = Math.max(tree[i], highTree);
+				highTree = Math.max(highTree,  tree[i]);
 			}
-
-			int odd = 0, even = 0;
-			for (int i = 0; i < N; i++) {
-				int diff = highTree - tree[i];
-				odd += diff % 2;
-				even += diff / 2;
+			
+			Arrays.sort(tree);
+			int one = 0; 
+			int two = 0;
+			for(int i = 0; i < N; i++) {
+				int difference = highTree - tree[i];
+				two += difference / 2;
+				one += difference % 2;
 			}
-
-			int result = odd + even;
-			if (odd < even) {
-				int gap = (even - odd) * 2;
-				int remain = (gap / 3) * 2;
-
-				if (gap % 3 == 2) {
-					remain += 2;
-				} else if (gap % 3 == 1)
-					remain += 1;
-				result = 2 * odd + remain;
-			} else if (odd - even > 1) {
-				result += odd - even - 1;
+			while(two - one >= 2) {
+				two -= 1;
+				one += 2;
 			}
-
-			sb.append(result + "\n");
-
+			
+			int result = 0;
+			if( two >= one) {
+				result = two*2;
+			}else {
+				result = one * 2 -1;
+			}
+			
+			sb.append("#"+test_case+" "+result+"\n");
 		}
-		bw.write(sb.toString());
-		bw.flush();
-		br.close();
-		bw.close();
+		System.out.println(sb);
 	}
 }
