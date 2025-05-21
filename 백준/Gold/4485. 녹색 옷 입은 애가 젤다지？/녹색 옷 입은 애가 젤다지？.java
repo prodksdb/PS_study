793f7f8d5b40bd2221f66a9015708a842562214a
prodksdb <150729023+prodksdb@users.noreply.github.com>
2025-05-21 16:15:43 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 148 ms, Memory: 17336 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -38,7 +38,6 @@ class Main {
 				break;
 			map = new int[N][N];
 			cost = new int[N][N];
-
 			for (int i = 0; i < N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
@@ -53,24 +52,26 @@ class Main {
 			}
 
 			int result = bfs();
-			System.out.println("Problem " + index + ": " + result);
+			sb.append("Problem " + index + ": " + result + "\n");
 		}
+		System.out.println(sb);
 	}
 
 	static int bfs() {
 		PriorityQueue<Point> pq = new PriorityQueue<>();
-		pq.offer(new Point(0, 0, map[0][0]));
+		pq.add(new Point(0, 0, map[0][0]));
 		cost[0][0] = map[0][0];
 
 		while (!pq.isEmpty()) {
-			Point cur = pq.poll();
-			int cx = cur.x;
-			int cy = cur.y;
-			int cc = cur.cost;
+			Point current = pq.poll();
+			int cx = current.x;
+			int cy = current.y;
+			int cc = current.cost;
 
 			for (int i = 0; i < 4; i++) {
 				int nx = cx + dx[i];
 				int ny = cy + dy[i];
+
 				if (nx < 0 || ny < 0 || nx >= N || ny >= N)
 					continue;
 				if (cost[nx][ny] > cost[cx][cy] + map[nx][ny]) {
@@ -78,9 +79,7 @@ class Main {
 					pq.add(new Point(nx, ny, cost[nx][ny]));
 				}
 			}
-
 		}
-
 		return cost[N - 1][N - 1];
 	}
 }
